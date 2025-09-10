34064f8a3d7f4bfb8edc9a2f79f491088a54253f
Sony Neto <62178478+SonyNeto@users.noreply.github.com>
2025-09-10 02:36:18 -0300

    Update accents_pattern.py

--- engine/accents_pattern.py
+++ engine/accents_pattern.py
@@ -4,11 +4,13 @@ def accent_2(hemi_syl: list, pre_note_1, pre_note_2, pre_note_3, accent_note, ca
         if '[' in syllable:
             if syl_index == -1:
                 if '[' in hemi_syl[-5] and '[' not in hemi_syl[-4] and '[' not in hemi_syl[-3] and '[' in hemi_syl[-2] and second_accent != None:
+                    hemi_syl[-2] = '<b>' + hemi_syl[-2] + '</b>'
                     hemi_syl[-2] += accent_note
                     hemi_syl[-1] += cadence_note
                 elif second:
                     accent_note = accent_note.replace(")", "", 1)
                     cadence_note = cadence_note.replace("(", "", 1)
+                    hemi_syl[-1] = '<b>' + hemi_syl[-1] + '</b>'
                     hemi_syl[-1] += accent_note + cadence_note
                 # elif '[' in hemi_syl[-4] and '[' not in hemi_syl[-3]:
                 #     accent_note = accent_note.replace(")", "", 1)
@@ -18,29 +20,36 @@ def accent_2(hemi_syl: list, pre_note_1, pre_note_2, pre_note_3, accent_note, ca
                 #     hemi_syl[-2] += accent_note
                 #     hemi_syl[-1] += cadence_note
                 elif '[' in hemi_syl[-2]:
+                    hemi_syl[-2] = '<b>' + hemi_syl[-2] + '</b>'
                     hemi_syl[-2] += accent_note
                     hemi_syl[-1] += cadence_note
                 elif pattern == 2:
+                    hemi_syl[-3] = '<b>' + hemi_syl[-3] + '</b>'
                     hemi_syl[-3] += cava_note
                     hemi_syl[-2] += accent_note
                     hemi_syl[-1] += cadence_note
                 else:
+                    hemi_syl[-3] = '<b>' + hemi_syl[-3] + '</b>'
                     hemi_syl[-3] += accent_note
                     hemi_syl[-2] += cava_note
                     hemi_syl[-1] += cadence_note
             elif syl_index == -2:
+                hemi_syl[-2] = '<b>' + hemi_syl[-2] + '</b>'
                 hemi_syl[-2] += accent_note
                 hemi_syl[-1] += cadence_note
             elif pattern == 2:
+                hemi_syl[-3] = '<b>' + hemi_syl[-3] + '</b>'
                 hemi_syl[-3] += cava_note
                 hemi_syl[-2] += accent_note
                 hemi_syl[-1] += cadence_note
             else:
+                hemi_syl[-3] = '<b>' + hemi_syl[-3] + '</b>'
                 hemi_syl[-3] += accent_note
                 hemi_syl[-2] += cava_note
                 hemi_syl[-1] += cadence_note
             break
         elif syl_index == -3:
+            hemi_syl[syl_index + 1] = '<b>' + hemi_syl[syl_index + 1] + '</b>'
             hemi_syl[syl_index + 1] += accent_note
             hemi_syl[-1] += cadence_note
         syl_index += -1
